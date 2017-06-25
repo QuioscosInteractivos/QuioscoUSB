dc4c643c4ea7c1a9615c92636ca96202772d9048
KaritoVR\Karito <dicavi_92>
2017-06-24 21:24:43 -0500

    Se elimina el widget de hora, se reemplaza por uno propio. Se cambian fuentes.

--- QuioscoUSB/src/app/titlebar/titlebar.component.ts
+++ QuioscoUSB/src/app/titlebar/titlebar.component.ts
@@ -12,8 +12,55 @@ export class TitlebarComponent implements OnInit {
   // Título de la vista
   @Input()
   sbTitle: string;
+  dtHour:any;
   
   ngOnInit() {
+    /*var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = this.checkTime(m);
+    s = this.checkTime(s);
+    this.dtHour= h + ":" + m + ":" + s;
+    var t = setTimeout(this.ngOnInit, 500);*/
+    this.CalcTime();
   }
+
+  CalcTime(){
+     var today = new Date();
+    var nuHour = today.getHours();
+    var nuMinutes = today.getMinutes();
+    var nuSeconds = today.getSeconds();
+    var sbTime = "a.m";
+    nuMinutes = this.checkTime(nuMinutes);
+
+    if(nuHour>12){
+      nuHour = nuHour-12;
+      sbTime= "p.m";
+    }
+    this.dtHour= nuHour + ":" + nuMinutes +" "+sbTime+"    "+ this.FormatDate(today);
+    var t = setTimeout(this.CalcTime, 500);
+  }
+
+ 
+checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+FormatDate(date) {
+  var arMonthNames = [
+    "Ene", "Feb", "Mar",
+    "Apr", "May", "Jun", "Jul",
+    "Ago", "Sep", "Oct",
+    "Nov", "Dic"
+  ];
+
+  var dtDay = date.getDate();
+  var dtMonthIndex = date.getMonth();
+  var dtYear = date.getFullYear();
+  var dtMonth = arMonthNames[dtMonthIndex];
+
+  return dtDay + '/' + dtMonth + '/' + dtYear;
+}
 
 }
